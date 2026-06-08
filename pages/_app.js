355ce845db3b25ee5d6591da5d61ba8e6ad86e21
Marvin0998 <marvin.pees@gmail.com>
2026-06-08 10:51:59 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Service Worker registrieren
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+          console.log('SW registriert:', reg.scope)
+          // Benachrichtigungen anfragen
+          if ('Notification' in window && Notification.permission === 'default') {
+            Notification.requestPermission()
+          }
+        })
+        .catch(err => console.log('SW Fehler:', err))
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
