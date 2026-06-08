ed21e12a270534e75f02c5063213a09558d20037
Marvin0998 <marvin.pees@gmail.com>
2026-06-08 18:00:54 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    // Service Worker registrieren
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js')
         .then(reg => {
-          console.log('SW registriert:', reg.scope)
-          // Benachrichtigungen anfragen
           if ('Notification' in window && Notification.permission === 'default') {
             Notification.requestPermission()
           }
@@ -17,5 +15,18 @@ export default function App({ Component, pageProps }) {
     }
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Elektro Pees" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1B52DD" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
